d6cc3ae64932df8a94d37b34d24a9706165f38da
Hunter Treadaway <huntertreadaway@Hunters-MacBook-Pro.local>
2019-11-12 13:23:01 -0800

    completed promise mini sprint

--- exercises/bare_minimum/callbackReview.js
+++ exercises/bare_minimum/callbackReview.js
@@ -6,13 +6,30 @@ var fs = require('fs');
 var request = require('request');
 
 // This function should retrieve the first line of the file at `filePath`
-var pluckFirstLineFromFile = function (filePath) {
+var pluckFirstLineFromFile = function (filePath, callback) {
   // TODO
+  fs.readFile(filePath, function(err, content) {
+    // console.log('something is happening');
+    if (err) {
+      // console.log('there was an error',
+      callback(err);
+    } else {
+      // console.log('you did something successfully: ',
+      callback(null, content.toString().split('\n')[0]); // this provides the first line (everything after content.)
+    }
+  });
 };
 
 // This function should retrieve the status code of a GET request to `url`
-var getStatusCode = function (url) {
+var getStatusCode = function (url, callback) {
   // TODO
+  request(url, function(err, response, body) {
+    if (err) {
+      callback(err);
+    } else {
+      callback(null, response.statusCode);
+    }
+  });
 };
 
 // Export these functions so we can test them and reuse them in later exercises
--- exercises/bare_minimum/promisification.js
+++ exercises/bare_minimum/promisification.js
@@ -1,7 +1,7 @@
 /**
  * Create the promise returning `Async` suffixed versions of the functions below,
  * Promisify them if you can, otherwise roll your own promise returning function
- */ 
+ */
 
 var fs = require('fs');
 var request = require('request');
@@ -27,7 +27,26 @@ var getGitHubProfile = function(user, callback) {
   });
 };
 
-var getGitHubProfileAsync; // TODO
+// var getGitHubProfileAsync = function(user) {
+//   var options = {
+//     url: 'https://api.github.com/users/' + user,
+//     headers: { 'User-Agent': 'request' },
+//     json: true  // will JSON.parse(body) for us
+//   };
+//   return new Promise(function(resolve, reject) {
+//     request.get(options, function(err, res, body) {
+//       if (err) {
+//         reject(err);
+//       } else if (body.message) {
+//         reject(new Error('Failed to get GitHub profile: ' + body.message));
+//       } else {
+//         resolve(body);
+//       }
+//     })
+//   })
+// }
+
+var getGitHubProfileAsync = Promise.promisify(getGitHubProfile);
 
 
 // (2) Asyncronous token generation
@@ -38,14 +57,14 @@ var generateRandomToken = function(callback) {
   });
 };
 
-var generateRandomTokenAsync; // TODO
+var generateRandomTokenAsync = Promise.promisify(generateRandomToken); // this magically makes the regular function a promise!
 
 
 // (3) Asyncronous file manipulation
 var readFileAndMakeItFunny = function(filePath, callback) {
   fs.readFile(filePath, 'utf8', function(err, file) {
     if (err) { return callback(err); }
-   
+
     var funnyFile = file.split('\n')
       .map(function(line) {
         return line + ' lol';
@@ -56,7 +75,22 @@ var readFileAndMakeItFunny = function(filePath, callback) {
   });
 };
 
-var readFileAndMakeItFunnyAsync; // TODO
+var readFileAndMakeItFunnyAsync = function(filePath) {
+  return new Promise(function(resolve, reject) {
+    fs.readFile(filePath, 'utf8', function(err, file) {
+      if (err) {
+        reject(err);
+      } else {
+        var funnyFile = file.split('\n')
+          .map(function(line) {
+            return line + ' lol';
+          })
+          .join('\n');
+        resolve(funnyFile);
+      }
+    });
+  });
+};
 
 // Export these functions so we can test them and reuse them in later exercises
 module.exports = {
